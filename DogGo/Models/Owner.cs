694c1ebb2942f07d4f79414bfd3b324c6f3c568e
tkylesh <tks2n@mtmail.mtsu.edu>
2023-01-10 15:07:18 -0600

    Server side validation using data attributes on models.

--- DogGo/Models/Owner.cs
+++ DogGo/Models/Owner.cs
@@ -1,14 +1,27 @@
-﻿                     using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace DogGo.Models
 {
     public class Owner
     {
         public int Id { get; set; }
+
+        [EmailAddress]
+        [Required]
         public string Email { get; set; }
+        [Required(ErrorMessage = "Hmmm... You should really add a name...")]
+        [MaxLength(35, ErrorMessage = "Name is too long")]
         public string Name { get; set; }
+        [Required]
+        [StringLength(55, MinimumLength = 5)]
         public string Address { get; set; }
+        [Required]
+        [DisplayName("Neighborhood")]
         public int NeighborhoodId { get; set; }
+        [Phone]
+        [DisplayName("Phone Number")]
         public string Phone { get; set; }
         public List<Dog> Dogs { get; set; }
 
--- DogGo/Models/Walks.cs
+++ DogGo/Models/Walks.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace DogGo.Models
 {
     public class Walks
     {
         public int Id { get; set; }
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}")]
         public DateTime Date { get; set; }
         public int Duration { get; set; }
         public int WalkerId { get; set; }
